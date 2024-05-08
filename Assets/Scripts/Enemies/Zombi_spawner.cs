5265e95fdca48dec8c83b2b2ae338789d51881e8
Vladurka <0986535843v@gmail.com>
2024-05-08 13:11:16 +0200

    .

--- Assets/Scripts/Enemies/Zombi_spawner.cs
+++ Assets/Scripts/Enemies/Zombi_spawner.cs
@@ -3,13 +3,15 @@ using UnityEngine;
 
 public class Zombi_spawner : MonoBehaviour, IServiceLocator
 {
+    [SerializeField] private GameObject[] _prefabs;
     [SerializeField] private Transform _spawPosition;
-    private int _startSpawnAmount = 5;
-    [SerializeField] private int _spawnAmount;
+    [SerializeField] private int _startSpawnAmount = 5;
     [SerializeField] private float _spawnInterval = 2.0f;
     [SerializeField] private float _intervalBeetweenWaves = 10.0f;
     [SerializeField] private float _angle;
     [SerializeField] private int _zombiAdd = 5;
+    private int _index = 0;
+    private int _spawnAmount;
     public void Init()
     { 
         _spawnAmount = _startSpawnAmount;
@@ -21,14 +23,10 @@ public class Zombi_spawner : MonoBehaviour, IServiceLocator
             yield return new WaitForSeconds(_intervalBeetweenWaves);
             while (_spawnAmount > 0)
             {
-                GameObject zombi = zombi_object_pool.Instance.GetObject();
-                if (zombi != null)
-                {
-                    zombi.transform.position = _spawPosition.position;
-                    zombi.transform.rotation = Quaternion.Euler(0, _angle, 0);
-                    zombi.SetActive(true);
-                    _spawnAmount--;
-                }
+                _index = Random.Range(0, _prefabs.Length);
+                Instantiate(_prefabs[_index], _spawPosition.position, Quaternion.Euler(0, _angle, 0));
+                _spawnAmount--;
+
                 yield return new WaitForSeconds(_spawnInterval);
             }
 
--- Assets/Scripts/Systems/EnteryPoint.cs
+++ Assets/Scripts/Systems/EnteryPoint.cs
@@ -4,7 +4,6 @@ public class EnteryPoint : MonoBehaviour
 {
     [SerializeField] private VolumeController _volumeController;
     [SerializeField] private BuildTower _buildTower;
-    [SerializeField] private zombi_object_pool _zombi_object_pool;
     [SerializeField] private Zombi_spawner _zombi_spawner;
     [SerializeField] private CointSystem _cointSystem;
     [SerializeField] private TimeController _timeController;
@@ -19,8 +18,6 @@ public class EnteryPoint : MonoBehaviour
     private void Init()
     {
         _volumeController.Init();
-        _zombi_object_pool.Init1();
-        _zombi_object_pool.Init2();
         _zombi_spawner.Init();
         _cointSystem.Init();
         _timeController.Init();
@@ -33,7 +30,6 @@ public class EnteryPoint : MonoBehaviour
         ServiceLocator.Initialize();
 
         ServiceLocator.Current.Register<VolumeController>(_volumeController);
-        ServiceLocator.Current.Register<zombi_object_pool>(_zombi_object_pool);
         ServiceLocator.Current.Register<Zombi_spawner>(_zombi_spawner);
         ServiceLocator.Current.Register<CointSystem>(_cointSystem);
         ServiceLocator.Current.Register<BuildTower>(_buildTower);
